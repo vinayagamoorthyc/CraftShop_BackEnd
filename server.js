0ee16b973a2c233a5c78a64632ede45209d27177
vinayagamoorthyc <sanjaiiti2709@gmail.com>
2024-08-01 23:15:17 +0530

    "changes"

--- server.js
+++ server.js
@@ -51,7 +51,7 @@ app.get("/getParticular/:id", (req, res)=>{
     .catch(err=>res.json(err))
   })
 
-  // ------------------------------ subscriber --------------------------------
+  // ------------------------------ subscriber ----------------------------------------
 
   app.get("/getSubscribers", (req,res)=>{
     SubscriberModel.find()
@@ -71,7 +71,7 @@ app.get("/getParticular/:id", (req, res)=>{
     .catch(err=>res.json(err))
 });
 
-  // ------------------------------ Reports -------------------------------------
+  // ------------------------------ Reports ------------------------------------------------
 
   app.get("/getReports", (req,res)=>{
     ReportModel.find()
